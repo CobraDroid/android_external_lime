c493638aa7a5b9310ce59e680bffd85451358b00
Jake Valletta (jakev) <javallet@gmail.com>
2014-03-20 20:17:27 -0400

    Initial commit for JB

---
+++ Android.mk
@@ -0,0 +1,16 @@
+ifneq ($(TARGET_SIMULATOR),true)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := eng
+LOCAL_SRC_FILES:= lime.c
+LOCAL_MODULE := lime
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_STATIC_LIBRARIES := liblime liblog
+
+include $(BUILD_EXECUTABLE)
+
+endif  # TARGET_SIMULATOR != true
+
